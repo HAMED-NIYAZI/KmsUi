43c212a03918c61643fb5093adf291896f826ee9
hn <hamed.niyazi5@gmail.com>
2022-11-27 21:28:23 +0330

    While creating tree view

--- src/app/components/CommonComponent/tinput/tinput.component.ts
+++ src/app/components/CommonComponent/tinput/tinput.component.ts
@@ -37,4 +37,18 @@ export class TinputComponent implements OnInit {
 
   }
 
+  ClickOnPlus() {
+    console.log("ClickOnPlus");
+    // var toggler = document.getElementsByClassName("parent");
+    // var i;
+
+    // for (i = 0; i < toggler.length; i++) {
+    //   toggler[i].addEventListener("click", function () {
+    //     this.parentElement.querySelector(".nested").classList.toggle("active");
+    //     this.classList.toggle("parent-down");
+    //   });
+    // }
+  }
+
+
 }
